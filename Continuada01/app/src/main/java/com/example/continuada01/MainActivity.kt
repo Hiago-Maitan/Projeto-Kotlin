3698091f27a019e04f1c4061e75951de99125d05
Hiago <Hiago-Maitan>
2020-09-09 00:51:19 -0300

    continuada01 feito

---
+++ Continuada01/app/src/main/java/com/example/continuada01/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.example.continuada01
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun calcular(componente: View) {
+        Toast.makeText(this, "calculando...", Toast.LENGTH_SHORT).show()
+
+        //transformando dados em inteiros
+        val usuario = et_usuario.text.toString()
+        val anoAtual = et_anoAtual.text.toString().toInt()
+        val idade = et_idade.text.toString().toInt()
+
+        var anoQueNasceu = anoAtual - idade
+
+        // validando ano e idade
+        if (anoAtual <= 2019 || anoAtual >=2021 || idade <= -1) {
+            tv_resultado.text = "Dados Inválidos"
+            tv_resultado.setTextColor(Color.RED)
+        } else if (anoQueNasceu >= 2000 && anoQueNasceu <= 2010) {
+            tv_resultado.text = "${usuario} nasceu no ano ${anoQueNasceu} e é Geração X"
+            tv_resultado.setTextColor(Color.BLUE)
+        } else if (anoQueNasceu >= 2011) {
+            tv_resultado.text = "${usuario} nasceu no ano ${anoQueNasceu} e é Geração Novinhos"
+            tv_resultado.setTextColor(Color.MAGENTA)
+        } else {
+            tv_resultado.text = "${usuario} nasceu no ano ${anoQueNasceu} e é Geração Idoso"
+            tv_resultado.setTextColor(Color.GREEN)
+        }
+
+
+    }
+
+}
